4aa8f1eaefa3565d7423ffa765abd049bf097ac5
Progressive Beast <progressivebeast@gmail.com>
2022-04-24 05:37:45 +0200

    Unit testing is_prime method

--- is_prime.py
+++ is_prime.py
@@ -1,11 +1,19 @@
 
+from ast import Try
+import errno
+
+
 def is_prime(number):
-    if(number < 2):
+    try:
+        if(number < 2):
             return False
-    else:            
-        for quotient in range(2, number//2 + 1):        
-            if (number % quotient == 0):
-                return False
-            
         else:
-            return True
+            for quotient in range(2, number//2 + 1):
+                if (number % quotient == 0):
+                    return False
+
+            else:
+                return True
+
+    except:
+        return False
---
+++ is_prime_test.py
@@ -0,0 +1,28 @@
+import unittest
+from is_prime import is_prime
+
+
+class TestIsPrime(unittest.TestCase):
+
+    def test_True(self):
+        self.assertTrue(is_prime(3))
+
+    def test_False(self):
+        self.assertFalse(is_prime(4))
+
+    def test_with_negative_number(self):
+        self.assertFalse(is_prime(-1))
+
+    def test_with_alphabetic_character(self):
+        self.assertFalse(is_prime('a'))
+
+    def test_with_special_character(self):
+        self.assertFalse(is_prime('#'))
+
+    def test_without_parameter(self):
+        with self.assertRaises(TypeError):
+            is_prime()
+
+
+if __name__ == '__main__':
+    unittest.main()
